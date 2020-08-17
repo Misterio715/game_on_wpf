768ef79bf286ce8e533c743c309dde3ff20dd456
Milirik <56610764+Milirik@users.noreply.github.com>
2020-08-17 14:23:21 +0300

    test_update_21

--- LightHeroes3/Units.cs
+++ LightHeroes3/Units.cs
@@ -29,6 +29,7 @@ namespace LightHeroes3
         public int b = 30;
         public int c = 330;
         public int d = 3430;
+        public int e = 4430;
         public Soldier() {
             this.damage = 1;
             this.hp = 10;
